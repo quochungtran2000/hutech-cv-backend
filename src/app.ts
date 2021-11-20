43963494339ee4576deeda2d4abdbe354bba5a80
Quoc hung <tranquochung6810@gmail.com>
2021-11-21 03:56:17 +0700

    save

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import "reflect-metadata";
+import { globalPrefix, PORT } from "./helpers/constants";
 import Express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import initialRouter from "./router";
 import dbConfig from "./helpers/dbConfig";
 import { createConnection } from "typeorm";
-import { globalPrefix, PORT } from "./helpers/constants";
 import multer from "multer";
 
 // import auth from "./middleware/auth.middleware";
--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -6,5 +6,6 @@ export const DB_PG_PORT = getEnv("DB_PG_PORT");
 export const DB_PG_USERNAME = getEnv("DB_PG_USERNAME");
 export const DB_PG_PASSWORD = getEnv("DB_PG_PASSWORD");
 export const DB_PG_NAME = getEnv("DB_PG_NAME");
+export const SEND_KEY = getEnv("SEND_KEY");
 export const globalPrefix = `api/v1`;
 export const USER_SECRET = "exam";
